234ab3359098e7de53900fcccfaddd1b3a2e9da3
Rishabh Raj Verma <rishabhraj4675@gmail.com>
2024-09-25 20:00:01 +0530

    committed

--- Backend/server.js
+++ Backend/server.js
@@ -10,8 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const port = process.env.PORT || 3000;
 
 
-app.listen(3000, () => {
+
+app.listen(port, () => {
   console.log("Server running on port 3000");
 })
