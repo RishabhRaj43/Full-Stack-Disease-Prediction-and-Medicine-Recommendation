1c807ab8e9d1fd8f6f7b7e751fd034759e28373b
Rishabh Raj Verma <rishabhraj4675@gmail.com>
2024-09-25 16:46:49 +0530

    First Commit

---
+++ Backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+})
